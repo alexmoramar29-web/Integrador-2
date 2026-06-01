33704adc39d1e005feb990633f70324d5e404c9e
Harold g. <haroldmontes068@gmail.com>
2026-06-01 14:21:05 -0600

    login medio hecho

--- Calculator/src/app/_layout.tsx
+++ Calculator/src/app/_layout.tsx
@@ -1,21 +1,11 @@
-import {useFonts} from "expo-font";
-import {Stack} from "expo-router";
-import {StatusBar} from "expo-status-bar";
-import {SafeAreaProvider} from "react-native-safe-area-context";
+import { Stack } from 'expo-router';
 
-const RootLayout = () => {
-  const [loaded] = useFonts({ SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf")});
-
-  if (loaded === false) { return null }
-  else { }
-
-  return(
-  <SafeAreaProvider>
-    <StatusBar style="auto" />
-    <Stack screenOptions={ {headerTitleStyle: {fontFamily: "SpaceMono"}, headerBackTitleStyle: {fontFamily: "SpaceMono"}}}>
-      <Stack.Screen name="index" options={ {title: "Home"}}/>
-      </Stack>
-  </SafeAreaProvider>
+export default function Layout() {
+  return (
+    <Stack
+      screenOptions={{
+        headerShown: false, // Esto elimina la barra blanca de "Home" por completo
+      }}
+    />
   );
 }
-export default RootLayout;
